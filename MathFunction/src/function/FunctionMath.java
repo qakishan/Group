d97cdfa5aabac782a1fe6ce7f7720dd0eeb6c4be
qakishan <121118010+qakishan@users.noreply.github.com>
2023-02-16 09:41:10 +0530

    commit

---
+++ MathFunction/src/function/FunctionMath.java
@@ -0,0 +1,18 @@
+package function;
+
+public class FunctionMath {
+	public static void main(String[] args)
+	{
+		int a =4000;
+		int b= 420;
+		int c=306;
+		int d =401;
+		int larfrmabc =(a>b) ? ((a>c)?a:c) : ((b>c)?b:c);
+		
+		int f= (larfrmabc>d)? larfrmabc : d;
+		System.out.println(f);
+		
+		
+	}
+
+}
